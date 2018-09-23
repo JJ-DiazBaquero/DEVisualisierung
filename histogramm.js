13af0fefe14487806d91f58da373aae2683bcf54
Juan José Díaz Baquero <juanjose21@live.com.co>
2018-09-23 16:36:53 +0200

    Add files via upload
    added dynamic change of main viz with change in brush

--- histogramm.js
+++ histogramm.js
@@ -1,12 +1,16 @@
-var erzeugenHistogramm = function(data){
+var histogramm;
+var gBrush;
+var xHistogramm;
+
+var erzeugenHistogramm = function(data, svgGraph){
     var svgHistogramm = d3.select("#histogramm"),
         widthHistogramm  = svgHistogramm.attr("width") - margin.left - margin.right,
         heightHistogramm = svgHistogramm.attr("height") - margin.top - margin.bottom;
 
-    var xHistogramm = d3.scaleLinear()
-        .range([0, width])
+    xHistogramm = d3.scaleLinear()
+        .range([0, widthHistogramm])
         .domain(d3.extent(data, function(d) {return d.Score;}));
-    var histogramm = d3.histogram()
+    histogramm = d3.histogram()
         .value(function(d) { return d.Score; })
         .domain(xHistogramm.domain())
         .thresholds(100);
@@ -38,4 +42,24 @@ var erzeugenHistogramm = function(data){
         .attr("class", "axis axis--y")
         .call(d3.axisLeft(yHistogramm));
 
+    var brush = d3.brushX()
+        .extent([[0, 0], [widthHistogramm, heightHistogramm]])
+        .on("brush end", brushed);
+
+    gBrush = gHistogramm.append("g")
+        .attr("class", "brush")
+        .call(brush);
+    gBrush.call(brush.move, [widthHistogramm/4 , widthHistogramm/2]);
+
+    function brushed() {
+        var s = d3.event.selection || xHistogramm.range();
+        //console.log(d3.brushSelection(gBrush.node()));
+        //console.log(s.map(xHistogramm.invert))
+        dispatch.call("brushMove");
+    }
+
 };
+
+var gebenBruschAuswahl = function(){
+    return d3.brushSelection(gBrush.node()).map(xHistogramm.invert);
+}
